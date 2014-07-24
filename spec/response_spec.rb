3bf15b6333cd10d18605b69e070f1f5315080d79
Jeremy Evans <code@jeremyevans.net>
2014-07-24 08:32:40 -0700

    Add specs for all response methods

--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -27,3 +27,102 @@ describe "cookie handling" do
     body.should == 'Hello'
   end
 end
+
+describe "response #[] and #[]=" do
+  it "should get/set headers" do
+    app do |r|
+      r.on do
+        response['foo'] = 'bar'
+        response['foo'] + response.headers['foo']
+      end
+    end
+
+    header('foo').should == "bar"
+    body.should == 'barbar'
+  end
+end
+
+describe "response #write" do
+  it "should add to body" do
+    app do |r|
+      r.on do
+        response.write 'a'
+        response.write 'b'
+      end
+    end
+
+    body.should == 'ab'
+  end
+end
+
+describe "response #finish" do
+  it "should set status to 404 if body has not been written to" do
+    app do |r|
+      r.on do
+        s, h, b = response.finish
+        "#{s}#{h['Content-Type']}#{b.length}"
+      end
+    end
+
+    body.should == '404text/html; charset=utf-80'
+  end
+
+  it "should set status to 200 if body has been written to" do
+    app do |r|
+      r.on do
+        response.write 'a'
+        s, h, b = response.finish
+        response.write "#{s}#{h['Content-Type']}#{b.length}"
+      end
+    end
+
+    body.should == 'a200text/html; charset=utf-81'
+  end
+
+  it "should not overwrite existing status" do
+    app do |r|
+      r.on do
+        response.status = 500
+        s, h, b = response.finish
+        "#{s}#{h['Content-Type']}#{b.length}"
+      end
+    end
+
+    body.should == '500text/html; charset=utf-80'
+  end
+end
+
+describe "response #redirect" do
+  it "should set location and status" do
+    app do |r|
+      r.on 'a' do
+        response.redirect '/foo', 303
+      end
+      r.on do
+        response.redirect '/bar'
+      end
+    end
+
+    status('/a').should == 303
+    status.should == 302
+    header('Location', '/a').should == '/foo'
+    header('Location').should == '/bar'
+  end
+end
+
+describe "response #empty?" do
+  it "should return whether the body is empty" do
+    app do |r|
+      r.on 'a' do
+        response['foo'] = response.empty?.to_s
+      end
+      r.on do
+        response.write 'a'
+        response['foo'] = response.empty?.to_s
+      end
+    end
+
+    header('foo', '/a').should == 'true'
+    header('foo').should == 'false'
+  end
+end
