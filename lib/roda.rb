e19913ebc17ee9d62671aa6f467d15a41dac43c1
Jeremy Evans <code@jeremyevans.net>
2014-07-29 13:30:11 -0700

    Add Rdoc for core roda code

--- lib/roda.rb
+++ lib/roda.rb
@@ -1,29 +1,48 @@
 require "rack"
 require "thread"
 
+# The main class for Roda.  Roda is built completely out of plugins, with the
+# default plugin being Roda::RodaPlugins::Base, so this class is mostly empty
+# except for some constants.
 class Roda
+  # Roda's version, always specified by a string in \d+\.\d+\.\d+ format.
   RodaVersion = '0.9.0'.freeze
 
+  # Error class raised by Roda
   class RodaError < StandardError; end
 
+  # Base class used for Roda requests.  The instance methods for this
+  # class are added by Roda::RodaPlugins::Base::RequestMethods, so this
+  # only contains the class methods.
   class RodaRequest < ::Rack::Request;
     @roda_class = ::Roda
 
     class << self
+      # Reference to the Roda class related to this request class.
       attr_accessor :roda_class
 
+      # Since RodaRequest is anonymously subclassed when Roda is subclassed,
+      # and then assigned to a constant of the Roda subclass, make inspect
+      # reflect the likely name for the class.
       def inspect
         "#{roda_class.inspect}::RodaRequest"
       end
     end
   end
 
+  # Base class used for Roda responses.  The instance methods for this
+  # class are added by Roda::RodaPlugins::Base::ResponseMethods, so this
+  # only contains the class methods.
   class RodaResponse < ::Rack::Response;
     @roda_class = ::Roda
 
     class << self
+      # Reference to the Roda class related to this response class.
       attr_accessor :roda_class
 
+      # Since RodaResponse is anonymously subclassed when Roda is subclassed,
+      # and then assigned to a constant of the Roda subclass, make inspect
+      # reflect the likely name for the class.
       def inspect
         "#{roda_class.inspect}::RodaResponse"
       end
@@ -35,18 +54,37 @@ class Roda
   @plugins = {}
   @opts = {}
 
+  # Module in which all Roda plugins should be stored.
   module RodaPlugins
+    # The base plugin for Roda, implementing all default functionality.
+    # Methods are put into a plugin so future plugins can easily override
+    # them and call super to get the default behavior.
     module Base
+      # Class methods for the Roda class.
       module ClassMethods
+        # Mutex protecting the plugins hash
         MUTEX = ::Mutex.new
 
+        # The rack application that this class uses.
         attr_reader :app
+
+        # The settings/options hash for the current class.
         attr_reader :opts
 
+        # Call the internal rack application with the given environment.
+        # This allows the class itself to be used as a rack application.
+        # However, for performance, it's better to use #app to get direct
+        # access to the underlying rack app.
         def call(env)
           app.call(env)
         end
 
+        # When inheriting Roda, setup a new rack app builder, copy the
+        # default middleware and opts into the subclass, and set the
+        # request and response classes in the subclasses to be subclasses
+        # of the request and responses classes in the parent class.  This
+        # makes it so child classes inherit plugins from their parent,
+        # but using plugins in child classes does not affect the parent.
         def inherited(subclass)
           super
           subclass.instance_variable_set(:@builder, ::Rack::Builder.new)
@@ -62,6 +100,9 @@ class Roda
           subclass.const_set(:RodaResponse, response_class)
         end
 
+        # Load a new plugin into the current class.  A plugin can be a module
+        # which is used directly, or a symbol represented a registered plugin
+        # which will be required and then used.
         def plugin(mixin, *args, &block)
           if mixin.is_a?(Symbol)
             mixin = load_plugin(mixin)
@@ -89,26 +130,40 @@ class Roda
           end
         end
 
+        # Include the given module in the request class. If a block
+        # is provided instead of a module, create a module using the
+        # the block.
         def request_module(mod = nil, &block)
           module_include(:request, mod, &block)
         end
     
+        # Include the given module in the response class. If a block
+        # is provided instead of a module, create a module using the
+        # the block.
         def response_module(mod = nil, &block)
           module_include(:response, mod, &block)
         end
 
+        # Setup route definitions for the current class, and build the
+        # rack application using the stored middleware.
         def route(&block)
           @middleware.each{|a, b| @builder.use(*a, &b)}
           @builder.run lambda{|env| new.call(env, &block)}
           @app = @builder.to_app
         end
 
+        # Add a middleware to use for the rack application.  Must be
+        # called before calling #route.
         def use(*args, &block)
           @middleware << [args, block]
         end
 
         private
 
+        # If the registered plugin already exists, use it.  Otherwise,
+        # require it and return it.  This raises a LoadError if such a
+        # plugin doesn't exist, or a RodaError if it exists but it does
+        # not register itself correctly.
         def load_plugin(name)
           h = Roda.instance_variable_get(:@plugins)
           unless plugin = MUTEX.synchronize{h[name]}
@@ -118,6 +173,7 @@ class Roda
           plugin
         end
 
+        # Backbone of the request_module and response_module support.
         def module_include(type, mod)
           if type == :response
             klass = self::RodaResponse
@@ -142,41 +198,56 @@ class Roda
           mod
         end
     
+        # Register the given plugin with Roda, so that it can be loaded using #plugin
+        # with a symbol.  Should be used by plugin files.
         def register_plugin(name, mod)
           h = Roda.instance_variable_get(:@plugins)
           MUTEX.synchronize{h[name] = mod}
         end
       end
 
+      # Instance methods for the Roda class.
       module InstanceMethods
+        # Create a request and response of the appopriate
+        # class, the instance_exec the route block with
+        # the request, handling any halts.
         def call(env, &block)
           @_request = self.class::RodaRequest.new(self, env)
           @_response = self.class::RodaResponse.new
           _route(&block)
         end
 
+        # The environment for the current request.
         def env
           request.env
         end
 
+        # The class-level options hash.  This should probably not be
+        # modified at the instance level.
         def opts
           self.class.opts
         end
 
+        # The instance of the request class related to this request.
         def request
           @_request
         end
 
+        # The instance of the response class related to this request.
         def response
           @_response
         end
 
+        # The session for the current request.  Raises a RodaError if
+        # a session handler has not been loaded.
         def session
           env["rack.session"] || raise(RodaError, "You're missing a session handler. You can get started by adding use Rack::Session::Cookie")
         end
 
         private
 
+        # Internals of #call, extracted so that plugins can override
+        # behavior after the request and response have been setup.
         def _route(&block)
           catch(:halt) do
             request.handle_on_result(instance_exec(@_request, &block))
@@ -185,34 +256,49 @@ class Roda
         end
       end
 
+      # Instance methods for RodaRequest, mostly related to handling routing
+      # for the request.
       module RequestMethods
         PATH_INFO = "PATH_INFO".freeze
         SCRIPT_NAME = "SCRIPT_NAME".freeze
         TERM = {:term=>true}.freeze
         SEGMENT = "([^\\/]+)".freeze
 
+        # The current captures for the request.  This gets modified as routing
+        # occurs.
         attr_reader :captures
 
+        # The Roda instance related to this request object.  Useful if routing
+        # methods need access to the scope of the Roda route block.
         attr_reader :scope
 
+        # Store the roda instance and environment.
         def initialize(scope, env)
           @scope = scope
           @captures = []
           super(env)
         end
 
+        # As request routing modifies SCRIPT_NAME and PATH_INFO, this exists
+        # as a helper method to get the full request of the path info.
         def full_path_info
           "#{env[SCRIPT_NAME]}#{env[PATH_INFO]}"
         end
 
+        # If this is not a GET method, returns immediately.  Otherwise, calls
+        # #is if there are any arguments, or #on if there are no arguments.
         def get(*args, &block)
           is_or_on(*args, &block) if get?
         end
 
+        # Immediately stop execution of the route block and return the given
+        # rack response array of status, headers, and body.
         def halt(response)
           _halt(response)
         end
 
+        # Handle #on block return values.  By default, if a string is given
+        # and the response is empty, use the string as the response body.
         def handle_on_result(result)
           res = response
           if result.is_a?(String) && res.empty?
@@ -220,15 +306,24 @@ class Roda
           end
         end
 
+        # Show information about current request, including request class,
+        # request method and full path.
         def inspect
           "#<#{self.class.inspect} #{env['REQUEST_METHOD']} #{full_path_info}>"
         end
 
+        # Adds TERM as the final argument and passes to #on, ensuring that
+        # there is only a match if #on has fully matched the path.
         def is(*args, &block)
           args << TERM
           on(*args, &block)
         end
 
+        # Attempts to match on all of the arguments.  If all of the
+        # arguments match, control is yielded to the block, and after
+        # the block returns, the rack response will be returned.
+        # If any of the arguments fails, ensures the request state is
+        # returned to that before matches were attempted.
         def on(*args, &block)
           try do
             # We stop evaluation of this entire matcher unless
@@ -251,31 +346,42 @@ class Roda
           end
         end
 
+        # If this is not a POST method, returns immediately.  Otherwise, calls
+        # #is if there are any arguments, or #on if there are no arguments.
         def post(*args, &block)
           is_or_on(*args, &block) if post?
         end
 
+        # The response related to the current request.
         def response
           scope.response
         end
 
+        # Immediately redirect to the given path.
         def redirect(path, status=302)
           response.redirect(path, status)
           _halt response.finish
         end
 
-        # If you want to halt the processing of an existing handler
-        # and continue it via a different handler.
+        # Call the given rack app with the environment and immediately return
+        # the response as the response for this request.
         def run(app)
           _halt app.call(env)
         end
 
         private
 
+        # Internal halt method, used so that halt can be overridden to handle
+        # non-rack response arrays, but internal code that always generates
+        # rack response arrays can use this for performance.
         def _halt(response)
           throw :halt, response
         end
 
+        # Attempts to match the pattern to the current path.  If there is no
+        # match, returns false without changes.  Otherwise, modifies
+        # SCRIPT_NAME to include the matched path, removes the matched
+        # path from PATH_INFO, and updates captures with any regex captures.
         def consume(pattern)
           matchdata = env[PATH_INFO].match(/\A(\/(?:#{pattern}))(\/|\z)/)
 
@@ -290,6 +396,8 @@ class Roda
           captures.concat(vars)
         end
 
+        # Backbone of the verb method support, calling #is if there are any
+        # arguments, or #on if there are none.
         def is_or_on(*args, &block)
           if args.empty?
             on(*args, &block)
@@ -298,6 +406,8 @@ class Roda
           end
         end
 
+        # Attempt to match the argument to the given request, handling
+        # common ruby types.
         def match(matcher)
           case matcher
           when String
@@ -317,6 +427,9 @@ class Roda
           end
         end
 
+        # Match any of the elements in the given array.  Return at the
+        # first match without evaluating future matches.  Returns false
+        # if no elements in the array match.
         def match_array(matcher)
           matcher.any? do |m|
             if matched = match(m)
@@ -329,18 +442,14 @@ class Roda
           end
         end
 
-
-        # A matcher for files with a certain extension.
-        #
-        # @example
-        #   # PATH_INFO=/style/app.css
-        #   on "style", extension("css") do |file|
-        #     res.write file # writes app
-        #   end
+        # Match files with the given extension.  Requires that the
+        # request path end with the extension.
         def match_extension(ext)
           consume("([^\\/]+?)\.#{ext}\\z")
         end
 
+        # Match by request method.  This can be an array if you want
+        # to match on multiple methods.
         def match_method(type)
           if type.is_a?(Array)
             type.any?{|t| match_method(t)}
@@ -349,38 +458,39 @@ class Roda
           end
         end
 
-        # Used to ensure that certain request parameters are present. Acts like a
-        # precondition / assertion for your route.
-        #
-        # @example
-        #   # POST with data like user[fname]=John&user[lname]=Doe
-        #   on "signup", param("user") do |atts|
-        #     User.create(atts)
-        #   end
+        # Match the given parameter if present, even if the parameter is empty.
+        # Adds any match to the captures.
         def match_param(key)
           if v = self[key]
             captures << v
           end
         end
 
+        # Match the given parameter if present and not empty.
+        # Adds any match to the captures.
         def match_param!(key)
           if (v = self[key]) && !v.empty?
             captures << v
           end
         end
 
+        # Match the given string to the request path.  Regexp escapes the
+        # string so that regexp metacharacters are not matched, and recognizes
+        # colon tokens for placeholders.
         def match_string(str)
           str = Regexp.escape(str)
           str.gsub!(/:\w+/, SEGMENT)
           consume(str)
         end
 
+        # Only match if the request path is empty, which usually indicates it
+        # has already been fully matched.
         def match_term(term)
           !(term ^ (env[PATH_INFO] == ""))
         end
 
-        # @private Used internally by #on to ensure that SCRIPT_NAME and
-        #          PATH_INFO are reset to their proper values.
+        # Yield to the given block, clearing any captures before
+        # yielding and restoring the SCRIPT_NAME and PATH_INFO on exit.
         def try
           script = env[SCRIPT_NAME]
           path = env[PATH_INFO]
@@ -397,16 +507,21 @@ class Roda
         end
       end
 
+      # Instance methods for RodaResponse
       module ResponseMethods
         CONTENT_LENGTH = "Content-Length".freeze
         CONTENT_TYPE = "Content-Type".freeze
         DEFAULT_CONTENT_TYPE = "text/html".freeze
         LOCATION = "Location".freeze
 
+        # The status code to use for the response.  If none is given, will use 200
+        # code for non-empty responses and a 404 code for empty responses.
         attr_accessor :status
 
+        # The hash of response headers for the current response.
         attr_reader :headers
 
+        # Set the default headers when creating a response.
         def initialize
           @status  = nil
           @headers = default_headers
@@ -414,45 +529,62 @@ class Roda
           @length  = 0
         end
 
+        # Return the response header with the given key.
         def [](key)
           @headers[key]
         end
 
+        # Set the response header with the given key to the given value.
         def []=(key, value)
           @headers[key] = value
         end
 
+        # Show response class, status code, response headers, and response body
         def inspect
           "#<#{self.class.inspect} #{@status.inspect} #{@headers.inspect} #{@body.inspect}>"
         end
 
+        # The default headers to use for responses.
         def default_headers
           {CONTENT_TYPE => DEFAULT_CONTENT_TYPE}
         end
 
+        # Modify the headers to include a Set-Cookie value that
+        # deletes the cookie.  A value hash can be provided to
+        # override the default one used to delete the cookie.
         def delete_cookie(key, value = {})
           ::Rack::Utils.delete_cookie_header!(@headers, key, value)
         end
 
+        # Whether the response body has been written to yet.  Note
+        # that writing an empty string to the response body marks
+        # the response as not empty.
         def empty?
           @body.empty?
         end
 
+        # Return the rack response array of status, headers, and body
+        # for the current response.
         def finish
           b = @body
           s = (@status ||= b.empty? ? 404 : 200)
           [s, @headers, b]
         end
 
+        # Set the Location header to the given path, and the status
+        # to the given status.
         def redirect(path, status = 302)
           @headers[LOCATION] = path
           @status  = status
         end
 
+        # Set the cookie with the given key in the headers.
         def set_cookie(key, value)
           ::Rack::Utils.set_cookie_header!(@headers, key, value)
         end
 
+        # Write to the response body.  Updates Content-Length header
+        # with the size of the string written.  Returns nil.
         def write(str)
           s = str.to_s
 
