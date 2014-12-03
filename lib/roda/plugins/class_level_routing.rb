89c69db74c7307d29959fd7711d2da3cea5e5aa1
Jeremy Evans <code@jeremyevans.net>
2014-12-03 15:48:27 -0800

    Add class_level_routing plugin, for a DSL similar to Sinatra
    This isn't really recommended for general use, but it does show
    how flexible Roda is.  It is a useful way to be able to add
    routes after defining the normal routing tree, though.

---
+++ lib/roda/plugins/class_level_routing.rb
@@ -0,0 +1,105 @@
+class Roda
+  module RodaPlugins
+    # The class_level_routing plugin adds routing methods at the class level, which can
+    # be used instead of or in addition to using the normal +route+ method to start the
+    # routing tree.  If a request is not matched by the normal routing tree, the class
+    # level routes will be tried.  This offers a more Sinatra-like API, while
+    # still allowing you to use a routing tree inside individual actions.
+    #
+    # Here's the first example from the README, modified to use the class_level_routing
+    # plugin:
+    #
+    #   class App < Roda
+    #     plugin :class_level_routing
+    #
+    #     # GET / request
+    #     root do
+    #       request.redirect "/hello"
+    #     end
+    #
+    #     # GET /hello/world request
+    #     get "hello/world" do
+    #       "Hello world!"
+    #     end
+    #
+    #     # /hello request
+    #     is "hello" do
+    #       # GET /hello request
+    #       request.get do
+    #         "Hello!"
+    #       end
+    #
+    #       # POST /hello request
+    #       request.post do
+    #         puts "Someone said hello!"
+    #         request.redirect
+    #       end
+    #     end
+    #   end
+    #
+    # When using the the class_level_routing plugin with nested routes, you may also want to use the
+    # delegate plugin to delegate certain instance methods to the request object, so you don't have
+    # to continually use +request.+ in your routing blocks.
+    #
+    # Note that class level routing is implemented via a simple array of routes, so routing performance
+    # will degrade linearly as the number of routes increases.  For best performance, you should use
+    # the normal +route+ class method to define your routing tree.  This plugin does make it simpler to
+    # add additional routes after the routing tree has already been defined, though.
+    module ClassLevelRouting
+      # Initialize the class_routes array when the plugin is loaded.  Also, if the application doesn't
+      # currently have a routing block, setup an empty routing block so that things will still work if
+      # a routing block isn't added.
+      def self.configure(app)
+        app.instance_eval do
+          route{} unless @route_block
+          @class_level_routes ||= []
+        end
+      end
+
+      module ClassMethods
+        # The array of class level routes for this application.
+        attr_reader :class_level_routes
+
+        # Define routing methods that will store class level routes.
+        [:root, :on, :is, :get, :post, :delete, :head, :options, :link, :patch, :put, :trace, :unlink].each do |meth|
+          define_method(meth) do |*args, &block|
+            @class_level_routes << [meth, args, block]
+          end
+        end
+
+        # Copy the class level routes into the subclass.
+        def inherited(subclass)
+          super
+          @subclass.instance_variable_set(:@class_level_routes, @class_level_routes.dup)
+        end
+      end
+
+      module InstanceMethods
+        private
+
+        # If the normal routing tree doesn't handle an action, try each class level route
+        # to see if it matches.
+        def _route(&block)
+          result = super
+
+          if result[0] == 404 && (v = result[2]).is_a?(Array) && v.empty?
+            # Reset the response so it doesn't inherit the status or any headers from
+            # the original response.
+            @_response = self.class::RodaResponse.new
+            super do |r|
+              self.class.class_level_routes.each do |meth, args, blk|
+                r.send(meth, *args) do |*a|
+                  instance_exec(*a, &blk)
+                end
+              end
+            end
+          else
+            result
+          end
+        end
+      end
+    end
+
+    register_plugin(:class_level_routing, ClassLevelRouting)
+  end
+end
---
+++ spec/plugin/class_level_routing_spec.rb
@@ -0,0 +1,123 @@
+require File.expand_path("spec_helper", File.dirname(File.dirname(__FILE__)))
+
+describe "class_level_routing plugin" do 
+  before do
+    app(:bare) do 
+      plugin :class_level_routing
+      plugin :all_verbs
+
+      root do
+        'root'
+      end
+
+      on "foo" do
+        request.get "bar" do
+          "foobar"
+        end
+
+        "foo"
+      end
+
+      is "d:d" do |x|
+        request.get do
+          "bazget#{x}"
+        end
+
+        request.post do
+          "bazpost#{x}"
+        end
+      end
+
+      meths = %w'get post delete head options patch put trace'
+      meths.concat(%w'link unlink') if ::Rack::Request.method_defined?("link?")
+      meths.each do |meth|
+        send(meth, :d) do |m|
+          "x-#{meth}-#{m}"
+        end
+      end
+    end
+  end
+
+  it "adds class methods for setting up routes" do
+    body.should == 'root'
+    body('/foo').should == 'foo'
+    body('/foo/bar').should == 'foobar'
+    body('/dgo').should == 'bazgetgo'
+    body('/dgo', 'REQUEST_METHOD'=>'POST').should == 'bazpostgo'
+    body('/bar').should == "x-get-bar"
+    body('/bar', 'REQUEST_METHOD'=>'POST').should == "x-post-bar"
+    body('/bar', 'REQUEST_METHOD'=>'DELETE').should == "x-delete-bar"
+    body('/bar', 'REQUEST_METHOD'=>'HEAD').should == "x-head-bar"
+    body('/bar', 'REQUEST_METHOD'=>'OPTIONS').should == "x-options-bar"
+    body('/bar', 'REQUEST_METHOD'=>'PATCH').should == "x-patch-bar"
+    body('/bar', 'REQUEST_METHOD'=>'PUT').should == "x-put-bar"
+    body('/bar', 'REQUEST_METHOD'=>'TRACE').should == "x-trace-bar"
+    if ::Rack::Request.method_defined?("link?")
+      body('/bar', 'REQUEST_METHOD'=>'LINK').should == "x-link-bar"
+      body('/bar', 'REQUEST_METHOD'=>'UNLINK').should == "x-unlink-bar"
+    end
+
+    status.should == 200
+    status("/asdfa/asdf").should == 404
+  end
+
+  it "only calls class level routes if routing tree doesn't handle request" do
+    app.route do |r|
+      r.root do
+        'iroot'
+      end
+
+      r.get 'foo' do
+        'ifoo'
+      end
+
+      r.on 'bar' do
+        r.get true do
+          response.status = 404
+          ''
+        end
+        r.post true do
+          'ibar'
+        end
+      end
+    end
+
+    body.should == 'iroot'
+    body('/foo').should == 'ifoo'
+    body('/foo/bar').should == 'foobar'
+    body('/dgo').should == 'bazgetgo'
+    body('/dgo', 'REQUEST_METHOD'=>'POST').should == 'bazpostgo'
+    body('/bar').should == ""
+    body('/bar', 'REQUEST_METHOD'=>'POST').should == "ibar"
+    body('/bar', 'REQUEST_METHOD'=>'DELETE').should == "x-delete-bar"
+    body('/bar', 'REQUEST_METHOD'=>'HEAD').should == "x-head-bar"
+    body('/bar', 'REQUEST_METHOD'=>'OPTIONS').should == "x-options-bar"
+    body('/bar', 'REQUEST_METHOD'=>'PATCH').should == "x-patch-bar"
+    body('/bar', 'REQUEST_METHOD'=>'PUT').should == "x-put-bar"
+    body('/bar', 'REQUEST_METHOD'=>'TRACE').should == "x-trace-bar"
+  end
+
+  it "works with the not_found plugin if loaded before" do
+    app.plugin :not_found do
+      "nf"
+    end
+
+    body.should == 'root'
+    body('/foo').should == 'foo'
+    body('/foo/bar').should == 'foobar'
+    body('/dgo').should == 'bazgetgo'
+    body('/dgo', 'REQUEST_METHOD'=>'POST').should == 'bazpostgo'
+    body('/bar').should == "x-get-bar"
+    body('/bar', 'REQUEST_METHOD'=>'POST').should == "x-post-bar"
+    body('/bar', 'REQUEST_METHOD'=>'DELETE').should == "x-delete-bar"
+    body('/bar', 'REQUEST_METHOD'=>'HEAD').should == "x-head-bar"
+    body('/bar', 'REQUEST_METHOD'=>'OPTIONS').should == "x-options-bar"
+    body('/bar', 'REQUEST_METHOD'=>'PATCH').should == "x-patch-bar"
+    body('/bar', 'REQUEST_METHOD'=>'PUT').should == "x-put-bar"
+    body('/bar', 'REQUEST_METHOD'=>'TRACE').should == "x-trace-bar"
+
+    status.should == 200
+    status("/asdfa/asdf").should == 404
+    body("/asdfa/asdf").should == "nf"
+  end
+end
