96cae43a436562b2da078a624489e3e77d6ce082
Jeremy Evans <code@jeremyevans.net>
2014-12-12 13:41:23 -0800

    Support :add_script_name, :name, :url, and :url_only options when creating named paths in the path plugin
    Unfortunately, due to how Roda works by default, :add_script_name
    is going to generate methods that give different results depending
    on where they are called in the routing tree.  You can use the
    static_path_info plugin to fix that (and it will be fixed by default
    in Roda 2).

--- lib/roda/plugins/path.rb
+++ lib/roda/plugins/path.rb
@@ -17,9 +17,31 @@ class Roda
     #       r.redirect bar_path(bar)
     #     end
     #   end
+    #
+    # The path method accepts the following options:
+    #
+    # :add_script_name :: Prefix the path generated with SCRIPT_NAME.
+    # :name :: Provide a different name for the method, instead of using <tt>*_path</tt>.
+    # :url :: Create a url method in addition to the path method, which will prefix the string generated
+    #         with the appropriate scheme, host, and port.  If true, creates a <tt>*_url</tt>
+    #         method.  If a Symbol or String, uses the value as the url method name.
+    # :url_only :: Do not create a path method, just a url method.
+    #
+    # Note that if :add_script_name, :url, or :url_only is used, this will also create a <tt>_*_path</tt>
+    # method.  This is necessary in order to support path methods that accept blocks, as you can't pass
+    # a block to a block that is instance_execed.
     module Path
+      DEFAULT_PORTS = {'http' => 80, 'https' => 443}.freeze
+
       module ClassMethods
-        def path(name, path=nil, &block)
+        # Create a new instance method for the named path.  See plugin module documentation for options.
+        def path(name, path=nil, opts={}, &block)
+          if path.is_a?(Hash)
+            raise RodaError,  "cannot provide two option hashses to Roda.path" unless opts.empty?
+            opts = path
+            path = nil
+          end
+
           raise RodaError,  "cannot provide both path and block to Roda.path" if path && block
           raise RodaError,  "must provide either path or block to Roda.path" unless path || block
 
@@ -28,7 +50,46 @@ class Roda
             block = lambda{path}
           end
 
-          define_method("#{name}_path", &block)
+          meth = opts[:name] || "#{name}_path"
+          url = opts[:url]
+          add_script_name = opts[:add_script_name]
+
+          if add_script_name || url || opts[:url_only]
+            _meth = "_#{meth}"
+            define_method(_meth, &block)
+          end
+
+          unless opts[:url_only]
+            if add_script_name
+              define_method(meth) do |*a, &blk|
+                request.script_name.to_s + send(_meth, *a, &blk)
+              end
+            else
+              define_method(meth, &block)
+            end
+          end
+
+          if url || opts[:url_only]
+            url_meth = if url.is_a?(String) || url.is_a?(Symbol)
+              url
+            else
+              "#{name}_url"
+            end
+
+            url_block = lambda do |*a, &blk|
+              r = request
+              scheme = r.scheme
+              port = r.port
+              uri = ["#{scheme}://#{r.host}#{":#{port}" unless DEFAULT_PORTS[scheme] == port}"]
+              uri << request.script_name.to_s if add_script_name
+              uri << send(_meth, *a, &blk)
+              File.join(uri)
+            end
+
+            define_method(url_meth, &url_block)
+          end
+
+          nil
         end
       end
     end
--- spec/plugin/path_spec.rb
+++ spec/plugin/path_spec.rb
@@ -1,6 +1,19 @@
 require File.expand_path("spec_helper", File.dirname(File.dirname(__FILE__)))
 
 describe "path plugin" do 
+  def path_app(*args, &block)
+    app(:bare) do
+      plugin :path
+      path *args, &block
+      route{|r| send(r.path_info)}
+    end
+  end
+
+  def path_block_app(b, *args, &block)
+    path_app(*args, &block)
+    app.route{|r| send(r.path_info, &b)}
+  end
+
   it "adds path method for defining named paths" do
     app(:bare) do
       plugin :path
@@ -8,13 +21,16 @@ describe "path plugin" do
       path :bar do |o|
         "/bar/#{o}"
       end
+      path :baz do |&block|
+        "/baz/#{block.call}"
+      end
 
       route do |r|
-        "#{foo_path}#{bar_path('a')}"
+        "#{foo_path}#{bar_path('a')}#{baz_path{'b'}}"
       end
     end
 
-    body.should == '/foo/bar/a'
+    body.should == '/foo/bar/a/baz/b'
   end
 
   it "raises if both path and block are given" do
@@ -25,5 +41,53 @@ describe "path plugin" do
   it "raises if neither path nor block are given" do
     app.plugin :path
     proc{app.path(:foo)}.should raise_error(Roda::RodaError)
+  end
+
+  it "raises if two options hashes are given" do
+    app.plugin :path
+    proc{app.path(:foo, {:name=>'a'}, :add_script_name=>true)}.should raise_error(Roda::RodaError)
+  end
+
+  it "supports :name option for naming the method" do
+    path_app(:foo, :name=>'foobar_route'){"/bar/foo"}
+    body("foobar_route").should == "/bar/foo"
+  end
+
+  it "supports :add_script_name option for automatically adding the script name" do
+    path_app(:foo, :add_script_name=>true){"/bar/foo"}
+    body("foo_path", 'SCRIPT_NAME'=>'/baz').should == "/baz/bar/foo"
+  end
+
+  it "supports path method accepting a block when using :add_script_name" do
+    path_block_app(lambda{"c"}, :foo, :add_script_name=>true){|&block| "/bar/foo/#{block.call}"}
+    body("foo_path", 'SCRIPT_NAME'=>'/baz').should == "/baz/bar/foo/c"
+  end
+
+  it "supports :url option for also creating a *_url method" do
+    path_app(:foo, :url=>true){"/bar/foo"}
+    body("foo_path", 'HTTP_HOST'=>'example.org', "rack.url_scheme"=>'http', 'SERVER_PORT'=>80).should == "/bar/foo"
+    body("foo_url", 'HTTP_HOST'=>'example.org', "rack.url_scheme"=>'http', 'SERVER_PORT'=>80).should == "http://example.org/bar/foo"
+  end
+
+  it "supports url method accepting a block when using :url" do
+    path_block_app(lambda{"c"}, :foo, :url=>true){|&block| "/bar/foo/#{block.call}"}
+    body("foo_url", 'HTTP_HOST'=>'example.org', "rack.url_scheme"=>'http', 'SERVER_PORT'=>80).should == "http://example.org/bar/foo/c"
+  end
+
+  it "supports url method name specified in :url option" do
+    path_app(:foo, :url=>:foobar_uri){"/bar/foo"}
+    body("foo_path", 'HTTP_HOST'=>'example.org', "rack.url_scheme"=>'http', 'SERVER_PORT'=>80).should == "/bar/foo"
+    body("foobar_uri", 'HTTP_HOST'=>'example.org', "rack.url_scheme"=>'http', 'SERVER_PORT'=>80).should == "http://example.org/bar/foo"
+  end
+
+  it "supports :url_only option for not creating a path method" do
+    path_app(:foo, :url_only=>true){"/bar/foo"}
+    proc{body("foo_path")}.should raise_error(NoMethodError)
+    body("foo_url", 'HTTP_HOST'=>'example.org', "rack.url_scheme"=>'http', 'SERVER_PORT'=>80).should == "http://example.org/bar/foo"
+  end
+
+  it "handles non-default ports in url methods" do
+    path_app(:foo, :url=>true){"/bar/foo"}
+    body("foo_url", 'HTTP_HOST'=>'example.org', "rack.url_scheme"=>'http', 'SERVER_PORT'=>81).should == "http://example.org:81/bar/foo"
   end
 end
