70a7442d6a0c221e1bbcee2789cf4b2cfdd66b7e
Jeremy Evans <code@jeremyevans.net>
2015-03-08 13:52:07 -0700

    Rename view_subdirs plugin to view_options, add support for branch/route specific view/layout options/locals
    This can make it easier to deal with branches or routes that want
    to use options different than the default plugin options.
    Previously, if you wanted something different, you had to pass
    options to every call to render/view where you wanted to change
    behavior.  That's not very DRY, and sort of defeats one of the
    main advantages of using a routing tree.
    
    With this plugin, you can set the options/locals to use for a
    given branch, and they will take precedence over the render
    plugin options, but they have lower precedence than options
    given directly to a view/render method.
    
    As view_options is a superset of view_subdirs, and attempts to
    load the view_subdirs plugin now end up loading view_options,
    there should be no effect on backwards compatibility.

---
+++ lib/roda/plugins/view_options.rb
@@ -0,0 +1,147 @@
+class Roda
+  module RodaPlugins
+    # The view_options plugin allows you to override view and layout
+    # options and locals for specific branches and routes.
+    #
+    #   plugin :render
+    #   plugin :view_options
+    #
+    #   route do |r|
+    #     r.on "users" do
+    #       set_layout_options :template=>'users_layout'
+    #       set_layout_locals :title=>'Users'
+    #       set_view_options :engine=>'haml'
+    #       set_view_locals :footer=>'(c) Roda'
+    #
+    #       # ...
+    #     end
+    #   end
+    #
+    # The options and locals you specify have higher precedence than
+    # the render plugin options, but lower precedence than options
+    # you directly pass to the view/render methods.
+    #
+    # The view_options plugin also has special support for sites
+    # that have outgrown a flat view directory and use subdirectories
+    # for views.  It allows you to set the view directory to
+    # use, and template names that do not contain a slash will
+    # automatically use that view subdirectory.  Example:
+    #
+    #   plugin :render, :layout=>'./layout'
+    #   plugin :view_options
+    #
+    #   route do |r|
+    #     r.on "users" do
+    #       set_view_subdir 'users'
+    #       
+    #       r.get :id do
+    #         view 'profile' # uses ./views/users/profile.erb
+    #       end
+    #
+    #       r.get 'list' do
+    #         view 'lists/users' # uses ./views/lists/users.erb
+    #       end
+    #     end
+    #   end
+    #
+    # Note that when a view subdirectory is set, the layout will
+    # also be looked up in the subdirectory unless it contains
+    # a slash.  So if you want to use a view subdirectory for
+    # templates but have a shared layout, you should make sure your
+    # layout contains a slash, similar to the example above.
+    module ViewOptions
+      # Load the render plugin before this plugin, since this plugin
+      # works by overriding methods in the render plugin.
+      def self.load_dependencies(app)
+        app.plugin :render
+      end
+
+      # The following methods are created via metaprogramming:
+      # set_layout_locals :: Set locals to use in the layout
+      # set_layout_options :: Set options to use when rendering the layout
+      # set_view_locals :: Set locals to use in the view
+      # set_view_options :: Set options to use when rendering the view
+      module InstanceMethods
+        %w'layout view'.each do |type|
+          %w'locals options'.each do |var|
+            v = "_#{type}_#{var}"
+            module_eval(<<-END, __FILE__, __LINE__+1)
+              def set#{v}(opts)
+                if @#{v}
+                  @#{v} = @#{v}.merge(opts)
+                else
+                  @#{v} = opts
+                end
+              end
+            END
+          end
+        end
+
+        # Set the view subdirectory to use.  This can be set to nil
+        # to not use a view subdirectory.
+        def set_view_subdir(v)
+          @_view_subdir = v
+        end
+
+        private
+
+        # If view options or locals have been set and this
+        # template isn't a layout template, merge the options
+        # and locals into the returned hash.
+        def parse_template_opts(template, opts)
+          t_opts = super
+
+          unless t_opts[:_is_layout]
+            if v_opts = @_view_options
+              t_opts.merge!(v_opts)
+            end
+
+            if v_locals = @_view_locals
+              t_opts[:locals] = if t_locals = t_opts[:locals]
+                v_locals.merge(t_locals)
+              else
+                v_locals
+              end
+            end
+          end
+
+          t_opts
+        end
+
+        # If layout options or locals have been set,
+        # merge the options and locals into the returned hash.
+        def render_layout_opts
+          opts = super
+
+          if l_opts = @_layout_options
+            opts.merge!(l_opts)
+          end
+
+          if l_locals = @_layout_locals
+            opts[:locals] = if o_locals = opts[:locals]
+              o_locals.merge(l_locals)
+            else
+              l_locals
+            end
+          end
+
+          opts
+        end
+
+        # Override the template name to use the view subdirectory if the
+        # there is a view subdirectory and the template name does not
+        # contain a slash.
+        def template_name(opts)
+          name = super
+          if (v = @_view_subdir) && name !~ /\//
+            "#{v}/#{name}"
+          else
+            name
+          end
+        end
+      end
+    end
+
+    register_plugin(:view_options, ViewOptions)
+  end
+end
--- lib/roda/plugins/view_subdirs.rb
+++ lib/roda/plugins/view_subdirs.rb
@@ -1,63 +1,6 @@
-class Roda
-  module RodaPlugins
-    # The view_subdirs plugin is designed for sites that have
-    # outgrown a flat view directory and use subdirectories
-    # for views.  It allows you to set the view directory to
-    # use, and template names that do not contain a slash will
-    # automatically use that view subdirectory.  Example:
-    #
-    #   plugin :render, :layout=>'./layout'
-    #   plugin :view_subdirs
-    #
-    #   route do |r|
-    #     r.on "users" do
-    #       set_view_subdir 'users'
-    #       
-    #       r.get :id do
-    #         view 'profile' # uses ./views/users/profile.erb
-    #       end
-    #
-    #       r.get 'list' do
-    #         view 'lists/users' # uses ./views/lists/users.erb
-    #       end
-    #     end
-    #   end
-    #
-    # Note that when a view subdirectory is set, the layout will
-    # also be looked up in the subdirectory unless it contains
-    # a slash.  So if you want to use a view subdirectory for
-    # templates but have a shared layout, you should make sure your
-    # layout contains a slash, similar to the example above.
-    module ViewSubdirs
-      # Load the render plugin before this plugin, since this plugin
-      # works by overriding a method in the render plugin.
-      def self.load_dependencies(app)
-        app.plugin :render
-      end
-
-      module InstanceMethods
-        # Set the view subdirectory to use.  This can be set to nil
-        # to not use a view subdirectory.
-        def set_view_subdir(v)
-          @_view_subdir = v
-        end
-
-        private
-
-        # Override the template name to use the view subdirectory if the
-        # there is a view subdirectory and the template name does not
-        # contain a slash.
-        def template_name(opts)
-          name = super
-          if (v = @_view_subdir) && name !~ /\//
-            "#{v}/#{name}"
-          else
-            name
-          end
-        end
-      end
-    end
-
-    register_plugin(:view_subdirs, ViewSubdirs)
-  end
-end
+# View options is a superset of the view_subdirs plugin,
+# which no longer exists.  For backwards compatibility,
+# make attempts to load the view_subdirs plugin load the
+# view_options plugin instead.
+require 'roda/plugins/view_options'
+Roda::RodaPlugins.register_plugin(:view_subdirs, Roda::RodaPlugins::ViewOptions)
