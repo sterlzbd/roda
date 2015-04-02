ecd640a35d0bd99d1de7ef84fa8a9e7b32e4761c
kematzy <kematzy@gmail.com>
2015-04-02 12:54:43 +0800

    Extracted partials() into separate plugin
    For those occasions when we want just the partials() support and not the Padrino render functionality.

---
+++ lib/roda/plugins/partials.rb
@@ -0,0 +1,46 @@
+class Roda
+  module RodaPlugins
+    # The partials plugin adds a +partial+ method, which renders 
+    # templates without the layout.
+    # 
+    #   plugin :partials, :views => 'path/2/views'
+    # 
+    # Note, this plugin automatically loads the :render plugin.
+    # 
+    # 
+    # Template files are prefixed with an underscore:
+    #
+    #   partial('test')     # uses _test.erb
+    #   partial('dir/test') # uses dir/_test.erb
+    #
+    # 
+    module RenderPartials
+      OPTS = {}.freeze
+      SLASH = '/'.freeze
+
+      # Depend on the render plugin, since this overrides
+      # some of its methods.
+      def self.load_dependencies(app, opts=OPTS)
+        app.plugin :render, opts
+      end
+
+      module InstanceMethods
+
+        # Renders the given template without a layout, but
+        # prefixes the template filename to use with an 
+        # underscore.
+        def partial(template, opts=OPTS)
+          opts = parse_template_opts(template, opts)
+          if opts[:template]
+            template = opts[:template].split(SLASH)
+            template[-1] = "_#{template[-1]}"
+            opts[:template] = template.join(SLASH)
+          end
+          render_template(opts)
+        end
+      end
+    end
+
+    register_plugin(:partials, RenderPartials)
+  end
+end
