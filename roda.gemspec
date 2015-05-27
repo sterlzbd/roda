66fc430c4625b02e01d0da2604d8e9c909790e48
Jeremy Evans <code@jeremyevans.net>
2015-05-27 14:17:15 -0700

    Add websockets plugin, for integration with faye-websocket
    The specs use thin for testing, since that was easy to setup
    and worked correctly.  I tried to get the specs working with
    puma, but unfortunately I was not able to do so.
    
    Thin doesn't install on JRuby, so I didn't put the thin dependency
    in the gemspec. In the travis gemfile install thin except when on
    JRuby, so that Travis should work correctly.  This will result in
    not testing the websockets plugin on JRuby, unfortunately.

--- roda.gemspec
+++ roda.gemspec
@@ -20,4 +20,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency "sass"
   s.add_development_dependency "json"
   s.add_development_dependency "mail"
+  s.add_development_dependency "faye-websocket"
 end
---
+++ spec/plugin/websockets_spec.rb
@@ -0,0 +1,78 @@
+require File.expand_path("spec_helper", File.dirname(File.dirname(__FILE__)))
+
+begin
+  lib = nil
+  for lib in %w'faye/websocket thin' do
+    require lib
+  end
+rescue LoadError
+  warn "#{lib} not installed, skipping websockets plugin test"  
+else
+describe "websockets plugin" do 
+  it "supports regular requests" do
+    app(:websockets) do |r|
+      r.websocket{}
+      "a"
+    end
+    body.must_equal 'a'
+  end
+end
+
+describe "websockets plugin" do 
+  before do
+    events = @events = []
+    app(:bare) do
+      plugin :websockets, :adapter=>:thin
+      route do |r|
+        r.websocket do |ws|
+          ws.on(:open) do |event|
+            events << 'open'
+          end
+          ws.on(:message) do |event|
+            events << event.data
+            ws.send(event.data.reverse)
+          end
+          ws.on(:close) do |event|
+            events << 'close'
+          end
+        end
+      end
+    end
+
+    @port = 9791
+    q = Queue.new
+    Thread.new do
+      Thin::Logging.silent = true
+      Rack::Handler.get('thin').run(app, :Port => @port) do |s|
+        @server = s
+        q.push nil
+      end
+    end
+    q.pop
+  end
+  after do
+    @server.stop
+  end
+
+  it "supports websocket requests" do
+    ws = Faye::WebSocket::Client.new("ws://localhost:#{@port}")
+    msg = nil
+    ws.on(:open){|event| msg = true}
+    t = Time.now
+    sleep 0.01 until msg || Time.now - t > 1
+    msg.must_equal true
+
+    msg = nil
+    ws.on(:message){|event| msg = event.data}
+    ws.send("hello")
+    t = Time.now
+    sleep 0.01 until msg || Time.now - t > 1
+    msg.must_equal 'olleh'
+
+    ws.close
+    t = Time.now
+    sleep 0.01 until @events == %w'open hello close' || Time.now - t > 2
+    @events.must_equal %w'open hello close'
+  end
+end
+end
