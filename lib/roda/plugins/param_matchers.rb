271a81ed7559532d26fcbc692d29fe121b22048b
Jeremy Evans <code@jeremyevans.net>
2015-09-05 07:14:33 -0700

    Add :params and :params! matchers to param_matchers plugin
    You can currently accomplish the same thing using multiple hash
    arguments:
    
      r.on({:param=>'foo'}, :param=>'bar')
    
    but this allows for nicer syntax:
    
      r.on :params=>%w'foo bar'

--- lib/roda/plugins/param_matchers.rb
+++ lib/roda/plugins/param_matchers.rb
@@ -4,7 +4,7 @@ class Roda
     # on the request's params.
     #
     # It adds a :param matcher for matching on any param with the
-    # same name, yielding the value of the param.
+    # same name, yielding the value of the param:
     #
     #   r.on :param => 'foo' do |value|
     #     # Matches '?foo=bar', '?foo='
@@ -12,16 +12,30 @@ class Roda
     #   end
     #
     # It adds a :param! matcher for matching on any non-empty param
-    # with the same name, yielding the value of the param.
+    # with the same name, yielding the value of the param:
     #
     #   r.on(:param! => 'foo') do |value|
     #     # Matches '?foo=bar'
     #     # Doesn't match '?foo=', '?bar=foo'
     #   end
+    #
+    # It also adds :params and :params! matchers, for matching multiple
+    # params at the same time:
+    #
+    #   r.on :params => ['foo', 'baz'] do |value|
+    #     # Matches '?foo=bar&baz=quuz', '?foo=&baz='
+    #     # Doesn't match '?foo=bar', '?baz='
+    #   end
+    #
+    #   r.on :params! => ['foo', 'baz'] do |value|
+    #     # Matches '?foo=bar&baz=quuz'
+    #     # Doesn't match '?foo=bar', '?baz=', '?foo=&baz=', '?foo=bar&baz='
+    #   end
+    #
     module ParamMatchers
       module RequestMethods
         # Match the given parameter if present, even if the parameter is empty.
-        # Adds any match to the captures.
+        # Adds match to the captures.
         def match_param(key)
           if v = self[key]
             @captures << v
@@ -29,10 +43,26 @@ class Roda
         end
 
         # Match the given parameter if present and not empty.
-        # Adds any match to the captures.
+        # Adds match to the captures.
         def match_param!(key)
           if (v = self[key]) && !v.empty?
             @captures << v
+          end
+        end
+
+        # Match all given parameters if present, even if any/all parameters is empty.
+        # Adds all matches to the captures.
+        def match_params(keys)
+          keys.each do |key|
+            return false unless match_param(key)
+          end
+        end
+
+        # Match all given parameters if present and not empty.
+        # Adds all matches to the captures.
+        def match_params!(keys)
+          keys.each do |key|
+            return false unless match_param!(key)
           end
         end
       end
--- spec/plugin/param_matchers_spec.rb
+++ spec/plugin/param_matchers_spec.rb
@@ -7,9 +7,7 @@ describe "param_matchers plugin" do
         email
       end
 
-      r.on do
-        "No email"
-      end
+      "No email"
     end
 
     io = StringIO.new
@@ -18,20 +16,54 @@ describe "param_matchers plugin" do
     body("/signup", "rack.input" => io, "QUERY_STRING" => "email=").must_equal 'No email'
   end
 
-  it "param matcheshould yield a param only if given" do
+  it "param matcher should yield a param only if given" do
     app(:param_matchers) do |r|
       r.get "signup", :param=>"email" do |email|
         email
       end
 
-      r.on do
-        "No email"
-      end
+      "No email"
     end
 
     io = StringIO.new
     body("/signup", "rack.input" => io, "QUERY_STRING" => "email=john@doe.com").must_equal 'john@doe.com'
     body("/signup", "rack.input" => io, "QUERY_STRING" => "").must_equal 'No email'
     body("/signup", "rack.input" => io, "QUERY_STRING" => "email=").must_equal ''
+  end
+
+  it "params! matcher should yield the params only if all are given and not empty" do
+    app(:param_matchers) do |r|
+      r.get "signup", :params! => %w"em ail" do |em, ail|
+        em + ail
+      end
+
+      "No email"
+    end
+
+    io = StringIO.new
+    body("/signup", "rack.input" => io, "QUERY_STRING" => "em=foo&ail=john@doe.com").must_equal 'foojohn@doe.com'
+    body("/signup", "rack.input" => io, "QUERY_STRING" => "em=&ail=john@doe.com").must_equal 'No email'
+    body("/signup", "rack.input" => io, "QUERY_STRING" => "em=foo&ail=").must_equal 'No email'
+    body("/signup", "rack.input" => io, "QUERY_STRING" => "em=&ail=").must_equal 'No email'
+    body("/signup", "rack.input" => io, "QUERY_STRING" => "em=foo").must_equal 'No email'
+    body("/signup", "rack.input" => io, "QUERY_STRING" => "ail=john@doe.com").must_equal 'No email'
+  end
+
+  it "params matcher should yield the params only if all are given" do
+    app(:param_matchers) do |r|
+      r.get "signup", :params=>%w"em ail" do |em, ail|
+        em + ail
+      end
+
+      "No email"
+    end
+
+    io = StringIO.new
+    body("/signup", "rack.input" => io, "QUERY_STRING" => "em=foo&ail=john@doe.com").must_equal 'foojohn@doe.com'
+    body("/signup", "rack.input" => io, "QUERY_STRING" => "em=&ail=john@doe.com").must_equal 'john@doe.com'
+    body("/signup", "rack.input" => io, "QUERY_STRING" => "em=foo&ail=").must_equal 'foo'
+    body("/signup", "rack.input" => io, "QUERY_STRING" => "em=&ail=").must_equal ''
+    body("/signup", "rack.input" => io, "QUERY_STRING" => "em=foo").must_equal 'No email'
+    body("/signup", "rack.input" => io, "QUERY_STRING" => "ail=john@doe.com").must_equal 'No email'
   end
 end
