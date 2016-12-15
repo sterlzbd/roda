7721d5ccb1625df226b1e2ffccf463f649c47e96
Jeremy Evans <code@jeremyevans.net>
2016-12-15 14:49:53 -0800

    Add handle_stream_error method to streaming plugin, for handling errors when using stream(:loop=>true)
    This uses a similar approach as the handle_chunk_error method
    in the chunk plugin.

--- lib/roda/plugins/streaming.rb
+++ lib/roda/plugins/streaming.rb
@@ -26,6 +26,25 @@ class Roda
     # :loop :: Whether to call the stream block continuously until
     #          the connection is closed.
     #
+    # If the :loop option is used, you can override the
+    # handle_stream_error method to change how exceptions
+    # are handled during streaming. This method is passed the
+    # exception and the stream.  By default, this method
+    # just reraises the exception, but you can choose to output
+    # the an error message to the stream, before raising:
+    #
+    #   def handle_stream_error(e, out)
+    #     out << 'ERROR!'
+    #     raise e
+    #   end
+    #
+    # Ignore errors completely while streaming:
+    #
+    #   def handle_stream_error(e, out)
+    #   end
+    #
+    # or handle the errors in some other way.
+    #
     # The implementation was originally taken from Sinatra,
     # which is also released under the MIT License:
     #
@@ -159,12 +178,21 @@ class Roda
           if opts[:loop]
             block = proc do |out|
               until out.closed?
-                yield(out)
+                begin
+                  yield(out)
+                rescue => e
+                  handle_stream_error(e, out)
+                end
               end
             end
           end
 
           throw :halt, @_response.finish_with_body(Stream.new(opts, &block))
+        end
+
+        # Handle exceptions raised while streaming when using :loop
+        def handle_stream_error(e, out)
+          raise e
         end
       end
     end
--- spec/plugin/streaming_spec.rb
+++ spec/plugin/streaming_spec.rb
@@ -49,4 +49,73 @@ describe "streaming plugin" do
     proc{b.each{|v| a << v}}.must_raise(Roda::RodaError)
     a.must_equal %w'a b e d'
   end
+
+  it "uses handle_stream_error for handling errors when streaming" do
+    a = []
+    app(:streaming) do |r|
+      b = %w'a b c'
+      stream(:loop=>true, :callback=>proc{a << 'e'}) do |out|
+        out << b.shift
+        raise Roda::RodaError, 'foo' if b.length == 1
+      end
+    end
+
+    app.send(:define_method, :handle_stream_error) do |error, out|
+      out << '1'
+      raise error
+    end
+
+    s, h, b = req
+    s.must_equal 200
+    h.must_equal('Content-Type'=>'text/html')
+    b.callback{a << 'd'}
+    proc{b.each{|v| a << v}}.must_raise(Roda::RodaError)
+    a.must_equal %w'a b 1 e d'
+  end
+
+  it "should allow closing the stream when handling an error" do
+    a = []
+    app(:streaming) do |r|
+      b = %w'a b c'
+      stream(:loop=>true, :callback=>proc{a << 'e'}) do |out|
+        out << b.shift
+        raise Roda::RodaError, 'foo' if b.length == 1
+      end
+    end
+
+    app.send(:define_method, :handle_stream_error) do |error, out|
+      out.close
+    end
+
+    s, h, b = req
+    s.must_equal 200
+    h.must_equal('Content-Type'=>'text/html')
+    b.callback{a << 'd'}
+    b.each{|v| a << v}
+    a.must_equal %w'a b e d'
+  end
+
+  it "should allow ignoring errors when streaming" do
+    a = []
+    b2 = %w'a b c'
+
+    app(:streaming) do |r|
+      stream(:loop=>true, :callback=>proc{a << 'e'}) do |out|
+        out << b2.shift
+        raise Roda::RodaError
+      end
+    end
+
+    app.send(:define_method, :handle_stream_error) do |error, out|
+      out << '1'
+      out.close if b2.empty?
+    end
+
+    s, h, b = req
+    s.must_equal 200
+    h.must_equal('Content-Type'=>'text/html')
+    b.callback{a << 'd'}
+    b.each{|v| a << v}
+    a.must_equal %w'a 1 b 1 c 1 e d'
+  end
 end
