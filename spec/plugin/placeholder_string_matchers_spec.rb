d595923d3a1c39f3379ecd4f170b988eb2d241ba
Jeremy Evans <code@jeremyevans.net>
2017-08-15 14:17:01 -0700

    Fix placeholder_string_matchers spec file naming and handle deprecation

--- spec/plugin/placeholder_string_matchers_spec.rb
+++ spec/plugin/placeholder_string_matchers_spec.rb
@@ -81,18 +81,6 @@ describe "placeholder_string_matchers plugin" do
           "d#{d}"
         end
 
-        r.is "foo:optd" do |o|
-          "foo#{o.inspect}"
-        end
-
-        r.is "bar:opt" do |o|
-          "bar#{o.inspect}"
-        end
-
-        r.is "format:format" do |f|
-          "format#{f.inspect}"
-        end
-
         r.is "thing/:thing" do |d|
           "thing#{d}"
         end
@@ -125,6 +113,38 @@ describe "placeholder_string_matchers plugin" do
     body("/a").must_equal 'wa'
     body("/1az0").must_equal 'w1az0'
     body("/f").must_equal 'ff'
+    body("/ffffffffffffffff").must_equal 'fffffffffffffffff'
+    status("/-").must_equal 404
+    body("/1/1a/f").must_equal 'dwf11af'
+    body("/12/1azy/fffff").must_equal 'dwf121azyfffff'
+    status("/1/f/a").must_equal 404
+    body("/qa/b/c/d//f/g").must_equal 'resta/b/c/d//f/g'
+    body('/q').must_equal 'rest'
+    body('/thing/q').must_equal 'thingq'
+    body('/thing2/q').must_equal 'thing2q'
+  end
+
+  deprecated "works with symbol_matchers plugin and deprecated matchers" do
+    app(:bare) do
+      plugin :placeholder_string_matchers
+      plugin :symbol_matchers
+      symbol_matcher(:f, /(f+)/)
+
+      route do |r|
+        r.is "foo:optd" do |o|
+          "foo#{o.inspect}"
+        end
+
+        r.is "bar:opt" do |o|
+          "bar#{o.inspect}"
+        end
+
+        r.is "format:format" do |f|
+          "format#{f.inspect}"
+        end
+      end
+    end
+
     body("/foo").must_equal 'foonil'
     body("/foo/123").must_equal 'foo"123"'
     status("/foo/bar").must_equal 404
@@ -135,14 +155,5 @@ describe "placeholder_string_matchers plugin" do
     body("/format").must_equal 'formatnil'
     body("/format.json").must_equal 'format"json"'
     status("/format.").must_equal 404
-    body("/ffffffffffffffff").must_equal 'fffffffffffffffff'
-    status("/-").must_equal 404
-    body("/1/1a/f").must_equal 'dwf11af'
-    body("/12/1azy/fffff").must_equal 'dwf121azyfffff'
-    status("/1/f/a").must_equal 404
-    body("/qa/b/c/d//f/g").must_equal 'resta/b/c/d//f/g'
-    body('/q').must_equal 'rest'
-    body('/thing/q').must_equal 'thingq'
-    body('/thing2/q').must_equal 'thing2q'
   end
 end
