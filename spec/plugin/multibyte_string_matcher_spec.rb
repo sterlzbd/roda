d8992a841f87aeae7ca7b001350ed14b4ddfba88
Jeremy Evans <code@jeremyevans.net>
2019-11-16 12:58:53 -0800

    Add multibyte_string_matcher plugin for matching multibyte characters
    multibyte characters are not commonly used in paths.  For one,
    they have to be URL escaped by browsers.  Roda previously handled
    multibyte characters in request paths correctly, but an
    optimization added in 3.19 broke the handling of multibyte
    characters.
    
    Introduce a plugin for this support, so that only people that need
    to handle multibyte characters in request paths pay the cost for
    the support.

---
+++ spec/plugin/multibyte_string_matcher_spec.rb
@@ -0,0 +1,44 @@
+require_relative "../spec_helper"
+
+describe "multibyte_string_matcher plugin" do 
+  it "uses multibyte safe string matching" do
+    str = "\xD0\xB8".force_encoding('UTF-8')
+    app(:unescape_path) do |r|
+      r.is String do |s|
+        s
+      end
+
+      r.is(Integer, /(#{str})/u) do |_, a|
+        a
+      end
+
+      r.is(Integer, Integer, str) do
+        'm'
+      end
+
+      r.is(Integer, str, Integer) do
+        'n'
+      end
+    end
+
+    body('/%D0%B8').must_equal str
+    body('/1/%D0%B8').must_equal str
+    status('/1/2/%D0%B8').must_equal 404
+    status('/1/%D0%B8/2').must_equal 404
+
+    status('/1/%D0%B9').must_equal 404
+    status('/1/2/%D0%B9').must_equal 404
+    status('/1/%D0%B9/2').must_equal 404
+
+    @app.plugin :multibyte_string_matcher
+
+    body('/%D0%B8').must_equal str
+    body('/1/%D0%B8').must_equal str
+    body('/1/2/%D0%B8').must_equal 'm'
+    body('/1/%D0%B8/2').must_equal 'n'
+
+    status('/1/%D0%B9').must_equal 404
+    status('/1/2/%D0%B9').must_equal 404
+    status('/1/%D0%B9/2').must_equal 404
+  end
+end
