f7efc9da59ffbf9a9e66057e087254f1aeec4c5b
Jeremy Evans <code@jeremyevans.net>
2020-01-22 08:55:31 -0800

    Dedup warnings if that is supported

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ $:.unshift(File.expand_path("../lib", File.dirname(__FILE__)))
 if ENV['WARNING']
   require 'warning'
   Warning.ignore([:missing_ivar, :method_redefined, :not_reached], File.dirname(File.dirname(__FILE__)))
+  Warning.dedup if Warning.respond_to?(:dedup)
 end
 
 if ENV['COVERAGE']
