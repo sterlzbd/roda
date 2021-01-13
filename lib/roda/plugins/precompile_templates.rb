82c7e6cb6a06d47febd0a10fa71235993961b7ac
Jeremy Evans <code@jeremyevans.net>
2021-01-13 12:45:12 -0800

    Add precompile_views and freeze_template_caches! to the precompile_templates plugin
    precompile_views precompiles the optimized render methods, so they
    can also be shared by child processes.
    
    freeze_template_caches! ensures that all templates are precompiled
    (raising an error if trying to compile a template afterward).  It
    also speeds up access to the caches by avoiding the use of a mutex
    when accessing them.

--- lib/roda/plugins/precompile_templates.rb
+++ lib/roda/plugins/precompile_templates.rb
@@ -13,32 +13,33 @@ class Roda
     # all of the child processes can use the same precompiled templates, which
     # saves memory.
     #
-    # After loading the plugin, you can call +precompile_templates+ with
-    # the pattern of templates you would like to precompile:
+    # Another advantage of the precompile_templates plugin is that after
+    # template precompilation, access to the template file in the file system is
+    # no longer needed, so this can be used with security features that do not
+    # allow access to the template files at runtime.
+    #
+    # After loading the plugin, you should call precompile_views with an array
+    # of views to compile, using the same argument you are passing to view or
+    # render:
     #
     #   plugin :precompile_templates
-    #   precompile_templates "views/\*\*/*.erb"
+    #   precompile_views %w'view1 view2'
+    #   
+    # If the view requires local variables, you should call precompile_views with a second
+    # argument for the local variables:
     #
-    # That will precompile all erb template files in the views directory or
-    # any subdirectory.
+    #   plugin :precompile_templates
+    #   precompile_views :view3, [:local_var1, :local_var2]
     #
-    # If the templates use local variables, you need to specify which local
-    # variables to precompile, which should be an array of symbols:
+    # After all templates are precompiled, you can optionally use freeze_template_caches!,
+    # which will freeze the template caches so that any template compilation at runtime
+    # will result in an error.  This also speeds up template cache access, since the
+    # template caches no longer need a mutex.
     #
-    #   precompile_templates 'views/users/_*.erb', locals: [:user]
+    #   freeze_template_caches!
     #
     # Note that you should use Tilt 2.0.1+ if you are using this plugin, so
     # that locals are handled in the same order.
-    #
-    # You can specify other render options when calling +precompile_templates+,
-    # including +:cache_key+, +:template_class+, and +:template_opts+.  If you
-    # are passing any of those options to render/view for the template, you
-    # should pass the same options when precompiling the template.
-    #
-    # To compile inline templates, just pass a single hash containing an :inline
-    # to +precompile_templates+:
-    #
-    #   precompile_templates inline: some_template_string
     module PrecompileTemplates
       # Load the render plugin as precompile_templates depends on it.
       def self.load_dependencies(app, opts=OPTS)
@@ -46,8 +47,49 @@ class Roda
       end
 
       module ClassMethods
-        # Precompile the templates using the given options.  See PrecompileTemplates
-        # for details.
+        # Freeze the template caches.  Should be called after precompiling all templates during
+        # application startup, if you don't want to allow templates to be cached at runtime.
+        # In addition to ensuring that no templates are compiled at runtime, this also speeds
+        # up rendering by freezing the template caches, so that a mutex is not needed to access
+        # them.
+        def freeze_template_caches!
+          _freeze_layout_method
+
+          opts[:render] = render_opts.merge(
+            :cache=>render_opts[:cache].freeze,
+            :template_method_cache=>render_opts[:template_method_cache].freeze,
+          ).freeze
+          self::RodaCompiledTemplates.freeze
+
+          nil
+        end
+
+        # Precompile the templates using the given options.  Note that this doesn't
+        # handle optimized template methods supported in newer versions of Roda, but
+        # there are still cases where makes sense to use it.
+        #
+        # You can call +precompile_templates+ with the pattern of templates you would
+        # like to precompile:
+        #
+        #   precompile_templates "views/**/*.erb"
+        #
+        # That will precompile all erb template files in the views directory or
+        # any subdirectory.
+        #
+        # If the templates use local variables, you need to specify which local
+        # variables to precompile, which should be an array of symbols:
+        #
+        #   precompile_templates 'views/users/_*.erb', locals: [:user]
+        #
+        # You can specify other render options when calling +precompile_templates+,
+        # including +:cache_key+, +:template_class+, and +:template_opts+.  If you
+        # are passing any of those options to render/view for the template, you
+        # should pass the same options when precompiling the template.
+        #
+        # To compile inline templates, just pass a single hash containing an :inline
+        # to +precompile_templates+:
+        #
+        #   precompile_templates inline: some_template_string
         def precompile_templates(pattern, opts=OPTS)
           if pattern.is_a?(Hash)
             opts = pattern.merge(opts)
@@ -68,7 +110,40 @@ class Roda
           instance = allocate
           compile_opts.each do |compile_opt|
             template = instance.send(:retrieve_template, compile_opt)
-            Render.tilt_template_compiled_method(template, locals, self)
+            begin
+              Render.tilt_template_compiled_method(template, locals, self)
+            rescue NotImplementedError
+              # When freezing template caches, you may want to precompile a template for a
+              # template type that doesn't support template precompilation, just to populate
+              # the cache.  Tilt rescues NotImplementedError in this case, which we can ignore.
+              nil
+            end
+          end
+
+          nil
+        end
+
+        # Precompile the given views with the given locals, handling optimized template methods.
+        def precompile_views(views, locals=EMPTY_ARRAY)
+          instance = allocate
+          views = Array(views)
+
+          if locals.empty?
+            opts = OPTS
+          else
+            locals_hash = {}
+            locals.each{|k| locals_hash[k] = nil}
+            opts = {:locals=>locals_hash}
+          end
+
+          views.each do |view|
+            instance.send(:retrieve_template, instance.send(:render_template_opts, view, opts))
+          end
+
+          if locals_hash
+            views.each do |view|
+              instance.send(:_optimized_render_method_for_locals, view, locals_hash)
+            end
           end
 
           nil
--- spec/plugin/precompile_templates_spec.rb
+++ spec/plugin/precompile_templates_spec.rb
@@ -5,7 +5,7 @@ begin
 rescue LoadError
   warn "tilt not installed, skipping precompiled_templates plugin test"  
 else
-describe "precompile_templates plugin" do 
+describe "precompile_templates plugin - precompile_templates method" do 
   it "adds support for template precompilation" do
     app(:bare) do
       plugin :render, :views=>'spec/views'
@@ -56,6 +56,95 @@ describe "precompile_templates plugin" do
     app.render_opts[:cache]['a'].instance_variable_get(:@compiled_method).length.must_equal 1
     body.strip.must_equal "a"
     app.render_opts[:cache]['a'].instance_variable_get(:@compiled_method).length.must_equal 1
+  end
+end
+
+describe "precompile_templates plugin - precompile_views method" do 
+  it "adds support for template precompilation without locals" do
+    app(:bare) do
+      plugin :render, :views=>'spec/views', :layout=>'layout-yield'
+      plugin :precompile_templates
+      route do |r|
+        @a = ' 1 '
+        view('iv')
+      end
+    end
+
+    app.render_opts[:cache][File.expand_path('spec/views/iv.erb')].must_be_nil
+    app.render_opts[:cache][File.expand_path('spec/views/layout-yield.erb')].must_be_nil
+    if Roda::RodaPlugins::Render::COMPILED_METHOD_SUPPORT
+      app.render_opts[:template_method_cache]['iv'].must_be_nil
+    end
+
+    app.precompile_views ['iv']
+    app.freeze_template_caches!
+    app.render_opts[:cache][File.expand_path('spec/views/iv.erb')].wont_be_nil
+    app.render_opts[:cache][File.expand_path('spec/views/layout-yield.erb')].wont_be_nil
+    app.render_opts[:cache].size.must_equal 2
+    if Roda::RodaPlugins::Render::COMPILED_METHOD_SUPPORT
+      app.render_opts[:template_method_cache]['iv'].wont_be_nil
+      app.render_opts[:template_method_cache].size.must_equal 2
+    end
+
+    body.strip.gsub(/\s+/, ' ').must_equal 'Header 1 Footer'
+    app.render_opts[:cache].size.must_equal 2
+    if Roda::RodaPlugins::Render::COMPILED_METHOD_SUPPORT
+      app.render_opts[:template_method_cache].size.must_equal 2
+    end
+  end
+
+  it "adds support for template precompilation with :locals" do
+    app(:bare) do
+      plugin :render, :views=>'spec/views', :layout=>false
+      plugin :precompile_templates
+      route do |r|
+        render('about', :locals=>{:title=>'1'})
+      end
+    end
+
+    app.render_opts[:cache][File.expand_path('spec/views/about.erb')].must_be_nil
+    if Roda::RodaPlugins::Render::COMPILED_METHOD_SUPPORT
+      app.render_opts[:template_method_cache]['about'].must_be_nil
+    end
+
+    app.precompile_views ['about'], [:title]
+    app.freeze_template_caches!
+    app.render_opts[:cache][File.expand_path('spec/views/about.erb')].wont_be_nil
+    app.render_opts[:cache].size.must_equal 1
+    if Roda::RodaPlugins::Render::COMPILED_METHOD_SUPPORT
+      app.render_opts[:template_method_cache][[:_render_locals, "about", [:title]]].wont_be_nil
+      app.render_opts[:template_method_cache].size.must_equal 1
+    end
+
+    body.strip.must_equal '<h1>1</h1>'
+    app.render_opts[:cache].size.must_equal 1
+    if Roda::RodaPlugins::Render::COMPILED_METHOD_SUPPORT
+      app.render_opts[:template_method_cache].size.must_equal 1
+    end
+  end
+end
+end
+
+begin
+  require 'tilt/sass'
+rescue LoadError
+  warn "tilt or sass not installed, skipping precompiled_templates plugin sass test"  
+else
+describe "precompile_templates plugin" do 
+  it "adds support for template precompilation for tilt template types that do not support precompilation" do
+    app(:bare) do
+      plugin :render, :views=>'spec/views'
+      plugin :precompile_templates
+      route do |r|
+        render(:path=>File.expand_path('spec/assets/css/app.scss'), :template_opts=>{:cache=>false})
+      end
+    end
+    key = [File.expand_path("spec/assets/css/app.scss"), nil, nil, {:cache=>false}, nil]
+    app.render_opts[:cache][key].must_be_nil
+    app.precompile_templates(:path=>File.expand_path('spec/assets/css/app.scss'), :template_opts=>{:cache=>false})
+    app.render_opts[:cache][key].wont_be_nil
+    app.freeze_template_caches!
+    body.must_match(/color: red;/)
   end
 end
 end
