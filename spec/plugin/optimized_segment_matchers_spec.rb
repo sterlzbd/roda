24353fc86958d6d0506be27fab27d97821c6caf3
Jeremy Evans <code@jeremyevans.net>
2021-05-07 09:28:35 -0700

    Add optimized_segment_matchers plugin for optimized matchers for a single String class argument
    This is similar to the optimized_string_matchers plugin, but for
    matching arbitrary segments instead of specific segments. It is
    designed for the case where the maximum performance is desired.

---
+++ spec/plugin/optimized_segment_matchers_spec.rb
@@ -0,0 +1,29 @@
+require_relative "../spec_helper"
+
+describe "optimized_segment_matchers plugin" do 
+  it "should support on_segment and is_segment match methods" do
+    app(:optimized_segment_matchers) do |r|
+      r.on_segment do |a|
+        r.is_segment do |b|
+          "x-#{a}-#{b}"
+        end
+
+        "y-#{a}"
+      end
+
+      "r"
+    end
+
+    body.must_equal 'r'
+    body('a').must_equal 'r'
+    body('/a').must_equal 'y-a'
+    body('/a/').must_equal 'y-a'
+    body('/b').must_equal 'y-b'
+    body('/b/').must_equal 'y-b'
+    body('/a/b').must_equal 'x-a-b'
+    body('/b/a').must_equal 'x-b-a'
+    body('/a/b/').must_equal 'y-a'
+    body('/a/b/c').must_equal 'y-a'
+    body('/a/b/c/').must_equal 'y-a'
+  end
+end
