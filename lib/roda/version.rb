0afce98f5920c2f3ea2f86e255cf0ed7fac77a84
Jeremy Evans <code@jeremyevans.net>
2021-05-12 08:34:24 -0700

    Bump version to 3.44.0

--- lib/roda/version.rb
+++ lib/roda/version.rb
@@ -4,11 +4,11 @@ class Roda
   RodaMajorVersion = 3
 
   # The minor version of Roda, updated for new feature releases of Roda.
-  RodaMinorVersion = 43
+  RodaMinorVersion = 44
 
   # The patch version of Roda, updated only for bug fixes from the last
   # feature release.
-  RodaPatchVersion = 1
+  RodaPatchVersion = 0
 
   # The full version of Roda as a string.
   RodaVersion = "#{RodaMajorVersion}.#{RodaMinorVersion}.#{RodaPatchVersion}".freeze
