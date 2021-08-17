e96f39f21bd67ffd488b51ebf89416e6b2d84a90
Jeremy Evans <code@jeremyevans.net>
2021-08-17 13:05:31 -0700

    Extract named_routes plugin from multi_route plugin
    This keeps the r.multi_route support in the multi_route plugin,
    but moves the support for r.route to a named_routes plugin. It
    has the multi_route plugin depend on the named_routes plugin.
    This way you don't need to load the multi_route plugin if you
    are just using the r.route support.
    
    One reason for this split is that the hash_routes plugin does not
    have a direct equivalent of just dispatching to a block for a named
    route.  So if you wanted to replace the multi_route plugin with the
    hash_routes plugin, it was more challenging if you were using
    r.route. Now you can switch from multi_route to hash_routes and
    named_routes.

--- lib/roda/plugins/multi_route.rb
+++ lib/roda/plugins/multi_route.rb
@@ -3,15 +3,10 @@
 #
 class Roda
   module RodaPlugins
-    # The multi_route plugin allows for multiple named routes, which the
-    # main route block can dispatch to by name at any point by calling +route+.
-    # If the named route doesn't handle the request, execution will continue,
-    # and if the named route does handle the request, the response returned by
-    # the named route will be returned.
-    #
-    # In addition, this plugin adds the +r.multi_route+ method, which will check
-    # if the first segment in the path matches a named route, and dispatch
-    # to that named route.
+    # The multi_route plugin builds on the named_routes plugin and allows for
+    # dispatching to multiple named routes # by calling the +r.multi_route+ method,
+    # which will check # if the first segment in the path matches a named route,
+    # and dispatch to that named route.
     #
     # The hash_routes plugin offers a +r.hash_routes+ method that is similar to
     # and performs better than the +r.multi_route+ method, and it is recommended
@@ -35,23 +30,14 @@ class Roda
     #
     #   route do |r|
     #     r.multi_route
-    #
-    #     # or
-    #
-    #     r.on "foo" do
-    #       r.route 'foo'
-    #     end
-    #
-    #     r.on "bar" do
-    #       r.route 'bar'
-    #     end
     #   end
     #
-    # Note that in multi-threaded code, you should not attempt to add a
-    # named route after accepting requests.
+    # Note that only named routes with string names will be dispatched to by the
+    # +r.multi_route+ method. Named routes with other names can be dispatched to
+    # using the named_routes plugin API, but will not be automatically dispatched
+    # to by +r.multi_route+.
     #
-    # If you want to use the +r.multi_route+ method, use string names for the
-    # named routes.  Also, you can provide a block to +r.multi_route+ that is
+    # You can provide a block to +r.multi_route+ that is
     # called if the route matches but the named route did not handle the
     # request:
     #
@@ -62,20 +48,10 @@ class Roda
     # If a block is not provided to multi_route, the return value of the named
     # route block will be used.
     #
-    # == Routing Files
-    #
-    # The convention when using the multi_route plugin is to have a single
-    # named route per file, and these routing files should be stored in
-    # a routes subdirectory in your application.  So for the above example, you
-    # would use the following files:
-    #
-    #   routes/bar.rb
-    #   routes/foo.rb
-    #
     # == Namespace Support
     #
     # The multi_route plugin also has support for namespaces, allowing you to
-    # use r.multi_route at multiple levels in your routing tree.  Example:
+    # use +r.multi_route+ at multiple levels in your routing tree.  Example:
     #
     #   route('foo') do |r|
     #     r.multi_route('foo')
@@ -103,81 +79,38 @@ class Roda
     #
     #   route do |r|
     #     r.multi_route
-    #
-    #     # or
-    #
-    #     r.on "foo" do
-    #       r.on("baz"){r.route("baz", "foo")}
-    #       r.on("quux"){r.route("quux", "foo")}
-    #     end
-    #
-    #     r.on "bar" do
-    #       r.on("baz"){r.route("baz", "bar")}
-    #       r.on("quux"){r.route("quux", "bar")}
-    #     end
     #   end
-    #
-    # === Routing Files
-    #
-    # The convention when using namespaces with the multi_route plugin is to
-    # store the routing files in subdirectories per namespace. So for the
-    # above example, you would have the following routing files:
-    #
-    #   routes/bar.rb
-    #   routes/bar/baz.rb
-    #   routes/bar/quux.rb
-    #   routes/foo.rb
-    #   routes/foo/baz.rb
-    #   routes/foo/quux.rb
     module MultiRoute
+      def self.load_dependencies(app)
+        app.plugin :named_routes
+      end
+
       # Initialize storage for the named routes.
       def self.configure(app)
-        app.opts[:namespaced_routes] ||= {}
         app::RodaRequest.instance_variable_set(:@namespaced_route_regexps, {})
       end
 
       module ClassMethods
-        # Freeze the namespaced routes and setup the regexp matchers so that there can be no thread safety issues at runtime.
+        # Freeze the multi_route regexp matchers so that there can be no thread safety issues at runtime.
         def freeze
-          opts[:namespaced_routes].freeze.each do |k,v|
-            v.freeze
+          super
+          opts[:namespaced_routes].each_key do |k|
             self::RodaRequest.named_route_regexp(k)
           end
           self::RodaRequest.instance_variable_get(:@namespaced_route_regexps).freeze
-          super
         end
 
         # Copy the named routes into the subclass when inheriting.
         def inherited(subclass)
           super
-          nsr = subclass.opts[:namespaced_routes]
-          opts[:namespaced_routes].each{|k, v| nsr[k] = v.dup}
           subclass::RodaRequest.instance_variable_set(:@namespaced_route_regexps, {})
         end
 
-        # The names for the currently stored named routes
-        def named_routes(namespace=nil)
-          unless routes = opts[:namespaced_routes][namespace]
-            raise RodaError, "unsupported multi_route namespace used: #{namespace.inspect}"
-          end
-          routes.keys
-        end
-
-        # Return the named route with the given name.
-        def named_route(name, namespace=nil)
-          opts[:namespaced_routes][namespace][name]
-        end
-
-        # If the given route has a name, treat it as a named route and
-        # store the route block.  Otherwise, this is the main route, so
-        # call super.
+        # Clear the multi_route regexp matcher for the namespace.
         def route(name=nil, namespace=nil, &block)
+          super
           if name
-            routes = opts[:namespaced_routes][namespace] ||= {}
-            routes[name] = define_roda_method(routes[name] || "multi_route_#{namespace}_#{name}", 1, &convert_route_block(block))
             self::RodaRequest.clear_named_route_regexp!(namespace)
-          else
-            super(&block)
           end
         end
       end
@@ -206,18 +139,13 @@ class Roda
         # is given, yield to the block.
         def multi_route(namespace=nil)
           on self.class.named_route_regexp(namespace) do |section|
-            r = route(section, namespace)
+            res = route(section, namespace)
             if block_given?
               yield
             else
-              r
+              res
             end
           end
-        end
-
-        # Dispatch to the named route with the given name.
-        def route(name, namespace=nil)
-          scope.send(roda_class.named_route(name, namespace), self)
         end
       end
     end
---
+++ spec/plugin/named_routes_spec.rb
@@ -0,0 +1,63 @@
+require_relative "../spec_helper"
+
+describe "named_routes plugin" do 
+  before do
+    app(:bare) do
+      plugin :named_routes
+
+      route(:p) do |r|
+        r.is do
+          'p'
+        end
+      end
+
+      route(:q, :b) do |r|
+        r.is do
+          'q'
+        end
+      end
+
+      route do |r|
+        r.on "p" do
+          r.route(:p)
+        end
+
+        r.on "q" do
+          r.route(:q, :b)
+        end
+      end
+    end
+  end
+
+  it "adds named routing support" do
+    body('/p').must_equal 'p'
+    body('/q').must_equal 'q'
+
+    status('/').must_equal 404
+    status('/b').must_equal 404
+    status('/p/').must_equal 404
+    status('/q/a').must_equal 404
+  end
+
+  it "works when freezing the app" do
+    app.freeze
+    body('/p').must_equal 'p'
+    body('/q').must_equal 'q'
+
+    status('/').must_equal 404
+    status('/b').must_equal 404
+    status('/p/').must_equal 404
+    status('/q/a').must_equal 404
+  end
+
+  it "works when subclassing the app" do
+    @app = Class.new(@app)
+    body('/p').must_equal 'p'
+    body('/q').must_equal 'q'
+
+    status('/').must_equal 404
+    status('/b').must_equal 404
+    status('/p/').must_equal 404
+    status('/q/a').must_equal 404
+  end
+end
