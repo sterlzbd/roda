2ab6f249bf86b670f7c2f9185dc91c97e0d1866a
Jeremy Evans <code@jeremyevans.net>
2021-11-01 11:19:37 -0700

    Update documentation for status_303 plugin
    Andrew on the Google Group points out there is actually a frontend
    framework requiring 303 and not supporting 302.

--- lib/roda/plugins/status_303.rb
+++ lib/roda/plugins/status_303.rb
@@ -6,8 +6,7 @@ class Roda
     # The status_303 plugin sets the default redirect status to be 303
     # rather than 302 when the request is not a GET and the
     # redirection occurs on an HTTP 1.1 connection as per RFC 7231.
-    # The author knows of no cases where this actually matters in
-    # practice.
+    # There are some frontend frameworks that require this behavior.
     #
     # Example:
     #
