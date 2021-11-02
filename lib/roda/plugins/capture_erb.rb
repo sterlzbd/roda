6bd99d43918ee53c8e8bdd9234dc2aa7f5091687
Jeremy Evans <code@jeremyevans.net>
2021-11-02 13:15:02 -0700

    Add capture_erb plugin for capturing ERB template blocks, instead of injecting them into the template output
    This can be combined with the inject_erb plugin to inject modified
    versions of captured blocks into template output.

---
+++ lib/roda/plugins/capture_erb.rb
@@ -0,0 +1,41 @@
+# frozen-string-literal: true
+
+#
+class Roda
+  module RodaPlugins
+    # The capture_erb plugin allows you to capture the content of a block
+    # in an ERB template, and return it as a value, instead of
+    # injecting the template block into the template output.
+    #
+    #   <% value = capture_erb do %>
+    #     Some content here.
+    #   <% end %>
+    #
+    # +capture_erb+ can be used inside other methods that are called
+    # inside templates.  It can be combined with the inject_erb plugin
+    # to wrap template blocks with arbitrary output and then inject the
+    # wrapped output into the template.
+    module CaptureERB
+      def self.load_dependencies(app)
+        app.plugin :render
+      end
+
+      module InstanceMethods
+        # Temporarily replace the ERB output buffer
+        # with an empty string, and then yield to the block.
+        # Return the value of the block, converted to a string.
+        # Restore the previous ERB output buffer before returning.
+        def capture_erb
+          outvar = render_opts[:template_opts][:outvar]
+          buf_was = instance_variable_get(outvar)
+          instance_variable_set(outvar, String.new)
+          yield.to_s
+        ensure
+          instance_variable_set(outvar, buf_was) if outvar && buf_was
+        end
+      end
+    end
+
+    register_plugin(:capture_erb, CaptureERB)
+  end
+end
---
+++ spec/plugin/capture_erb_spec.rb
@@ -0,0 +1,47 @@
+require_relative "../spec_helper"
+
+begin
+  require 'tilt/erb'
+rescue LoadError
+  warn "tilt not installed, skipping capture_erb plugin test"
+else
+describe "capture_erb plugin" do
+  before do
+    app(:bare) do
+      plugin :render, :views => './spec/views'
+      plugin :capture_erb
+      plugin :inject_erb
+
+      route do |r|
+        r.root do
+          render(:inline => "<% value = capture_erb do %>foo<% end %>bar<%= value %>")
+        end
+        r.get 'rescue' do
+          render(:inline => "<% value = capture_erb do %>foo<% raise %><% end rescue (value = 'baz') %>bar<%= value %>")
+        end
+        r.get 'inject' do
+          render(:inline => "<% some_method do %>foo<% end %>")
+        end
+      end
+
+      def some_method(&block)
+        inject_erb "bar"
+        inject_erb capture_erb(&block).upcase
+        inject_erb "baz"
+      end
+    end
+  end
+
+  it "should capture erb output" do
+    body.strip.must_equal "barfoo"
+  end
+
+  it "should handle exceptions in captured blocks" do
+    body('/rescue').strip.must_equal "barbaz"
+  end
+
+  it "should work with the inject_erb plugin" do
+    body('/inject').strip.must_equal "barFOObaz"
+  end
+end
+end
