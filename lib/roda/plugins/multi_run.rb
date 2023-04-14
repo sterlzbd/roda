4036c9392047937b2a483241e8500acacdd3af34
Jeremy Evans <code@jeremyevans.net>
2023-04-14 14:42:29 -0700

    Make Roda.run in multi_run plugin accept blocks to allow autoloading the apps to dispatch to
    This is being added for similar reasons as autoload_hash_branches
    and autoload_named_routes.

--- lib/roda/plugins/multi_run.rb
+++ lib/roda/plugins/multi_run.rb
@@ -17,7 +17,7 @@ class Roda
     #   App.run "ro", OtherRodaApp
     #   App.run "si", SinatraApp
     #
-    # Inside your route block, you can call r.multi_run to dispatch to all
+    # Inside your route block, you can call +r.multi_run+ to dispatch to all
     # three rack applications based on the prefix:
     #
     #   App.route do |r|
@@ -28,7 +28,7 @@ class Roda
     # starting with +/ro+ to +OtherRodaApp+, and routes starting with +/si+ to
     # SinatraApp.
     #
-    # You can pass a block to +multi_run+ that will be called with the prefix,
+    # You can pass a block to +r.multi_run+ that will be called with the prefix,
     # before dispatching to the rack app:
     #
     #   App.route do |r|
@@ -39,12 +39,26 @@ class Roda
     #
     # This is useful for modifying the environment before passing it to the rack app.
     #
-    # The multi_run plugin is similar to the multi_route plugin, with the difference
-    # being the multi_route plugin keeps all routing subtrees in the same Roda app/class,
-    # while multi_run dispatches to other rack apps.  If you want to isolate your routing
-    # subtrees, multi_run is a better approach, but it does not let you set instance
-    # variables in the main Roda app and have those instance variables usable in
-    # the routing subtrees.
+    # You can also call +Roda.run+ with a block:
+    #
+    #   App.run("ra"){PlainRackApp}
+    #   App.run("ro"){OtherRodaApp}
+    #   App.run("si"){SinatraApp}
+    #
+    # When called with a block, Roda will call the block to get the app to dispatch to
+    # every time the block is called.  The expected usage is with autoloaded classes,
+    # so that the related classes are not loaded until there is a request for the
+    # related route.  This can sigficantly speedup startup or testing a subset of the
+    # application.  When freezing an application, the blocks are called once to get the
+    # app to dispatch to, and that is cached, to ensure the any autoloads are completed
+    # before the application is frozen.
+    #
+    # The multi_run plugin is similar to the hash_branches and multi_route plugins, with
+    # the difference being the hash_branches and multi_route plugins keep all routing
+    # subtrees in the same Roda app/class, while multi_run dispatches to other rack apps.
+    # If you want to isolate your routing subtrees, multi_run is a better approach, but
+    # it does not let you set instance variables in the main Roda app and have those
+    # instance variables usable in the routing subtrees.
     #
     # To handle development environments that reload code, you can call the
     # +run+ class method without an app to remove dispatching for the prefix.
@@ -52,12 +66,21 @@ class Roda
       # Initialize the storage for the dispatched applications
       def self.configure(app)
         app.opts[:multi_run_apps] ||= {}
+        app.opts[:multi_run_app_blocks] ||= {}
       end
 
       module ClassMethods
+        # Convert app blocks into apps by calling them, in order to force autoloads
+        # and to speed up subsequent calls.
         # Freeze the multi_run apps so that there can be no thread safety issues at runtime.
         def freeze
-          opts[:multi_run_apps].freeze
+          app_blocks = opts[:multi_run_app_blocks]
+          apps = opts[:multi_run_apps]
+          app_blocks.each do |prefix, block|
+            apps[prefix] = block.call
+          end
+          app_blocks.clear.freeze
+          apps.freeze
           self::RodaRequest.refresh_multi_run_regexp!
           super
         end
@@ -69,12 +92,22 @@ class Roda
         end
 
         # Add a rack application to dispatch to for the given prefix when
-        # r.multi_run is called.
-        def run(prefix, app=nil)
-          if app
-            multi_run_apps[prefix.to_s] = app
+        # r.multi_run is called. If a block is given, it is called every time
+        # there is a request for the route to get the app to call. If neither
+        # a block or an app is provided, any stored route for the prefix is
+        # removed.  It is an error to provide both an app and block in the same call.
+        def run(prefix, app=nil, &block)
+          prefix = prefix.to_s
+          if app 
+            raise Roda::RodaError, "cannot provide both app and block to Roda.run" if block
+            opts[:multi_run_apps][prefix] = app
+            opts[:multi_run_app_blocks].delete(prefix)
+          elsif block
+            opts[:multi_run_apps].delete(prefix)
+            opts[:multi_run_app_blocks][prefix] = block
           else
-            multi_run_apps.delete(prefix.to_s)
+            opts[:multi_run_apps].delete(prefix)
+            opts[:multi_run_app_blocks].delete(prefix)
           end
           self::RodaRequest.refresh_multi_run_regexp!
         end
@@ -84,7 +117,7 @@ class Roda
         # Refresh the multi_run_regexp, using the stored route prefixes,
         # preferring longer routes before shorter routes.
         def refresh_multi_run_regexp!
-          @multi_run_regexp = /(#{Regexp.union(roda_class.multi_run_apps.keys.sort.reverse)})/
+          @multi_run_regexp = /(#{Regexp.union((roda_class.opts[:multi_run_apps].keys + roda_class.opts[:multi_run_app_blocks].keys).sort.reverse)})/
         end
 
         # Refresh the multi_run_regexp if it hasn't been loaded yet.
@@ -95,11 +128,12 @@ class Roda
 
       module RequestMethods
         # If one of the stored route prefixes match the current request,
-        # dispatch the request to the stored rack application.
+        # dispatch the request to the appropriate rack application.
         def multi_run
           on self.class.multi_run_regexp do |prefix|
             yield prefix if defined?(yield)
-            run scope.class.multi_run_apps[prefix]
+            opts = scope.opts
+            run(opts[:multi_run_apps][prefix] || opts[:multi_run_app_blocks][prefix].call)
           end
         end
       end
--- spec/plugin/multi_run_spec.rb
+++ spec/plugin/multi_run_spec.rb
@@ -48,6 +48,14 @@ describe "multi_run plugin" do
     proc{app.run "a", Class.new(Roda).class_eval{route{"a1"}; app}}.must_raise
   end
 
+  it "supports multi_run_apps" do
+    app(:multi_run){|r|}
+    app.multi_run_apps.must_equal({})
+    a = Class.new(Roda).class_eval{route{"a1"}; app}
+    app.run :a, a
+    app.multi_run_apps.must_equal('a'=>a)
+  end
+
   it "works when subclassing" do
     app(:multi_run) do |r|
       r.multi_run
@@ -95,5 +103,145 @@ describe "multi_run plugin" do
     body("/a").must_equal 'a1'
     app.run "a"
     body("/a").must_equal 'c'
+  end
+end
+
+describe "multi_run plugin with blocks for Roda.run" do 
+  it "adds Roda.run method for setting up prefix delegations to other rack apps" do
+    app(:multi_run) do |r|
+      r.multi_run
+      "c"
+    end
+
+    loaded = []
+    app.run("a"){loaded << :a1; Class.new(Roda).class_eval{route{"a1"}; app}}
+
+    body("/a").must_equal 'a1'
+    body("/b").must_equal 'c'
+    body("/b/a").must_equal 'c'
+    body.must_equal 'c'
+    loaded.must_equal [:a1]
+
+    app.run("b"){loaded << :b1; Class.new(Roda).class_eval{route{"b1"}; app}}
+
+    body("/a").must_equal 'a1'
+    body("/b").must_equal 'b1'
+    body("/b/a").must_equal 'b1'
+    body.must_equal 'c'
+    loaded.must_equal [:a1, :a1, :b1, :b1]
+
+    app.run("b/a"){loaded << :b2; Class.new(Roda).class_eval{route{"b2"}; app}}
+
+    body("/a").must_equal 'a1'
+    body("/b").must_equal 'b1'
+    body("/b/a").must_equal 'b2'
+    body.must_equal 'c'
+    loaded.must_equal [:a1, :a1, :b1, :b1, :a1, :b1, :b2]
+  end
+
+  it "works when freezing the app" do
+    app(:multi_run) do |r|
+      r.multi_run
+      "c"
+    end
+
+    app.run("a"){Class.new(Roda).class_eval{route{"a1"}; app}}
+    app.run("b"){Class.new(Roda).class_eval{route{"b1"}; app}}
+    app.run("b/a"){Class.new(Roda).class_eval{route{"b2"}; app}}
+    app.freeze
+
+    body("/a").must_equal 'a1'
+    body("/b").must_equal 'b1'
+    body("/b/a").must_equal 'b2'
+    body.must_equal 'c'
+
+    proc{app.run "a", Class.new(Roda).class_eval{route{"a1"}; app}}.must_raise
+  end
+
+  it "works when subclassing" do
+    app(:multi_run) do |r|
+      r.multi_run
+      "c"
+    end
+
+    app.run("a"){Class.new(Roda).class_eval{route{"a1"}; app}}
+    body("/a").must_equal 'a1'
+
+    a = app
+    @app = Class.new(a)
+
+    a.run("b"){Class.new(Roda).class_eval{route{"b2"}; app}}
+    app.run("b"){Class.new(Roda).class_eval{route{"b1"}; app}}
+
+    body("/a").must_equal 'a1'
+    body("/b").must_equal 'b1'
+
+    @app = a
+    body("/b").must_equal 'b2'
+  end
+
+  it "yields prefix" do
+    yielded = false
+
+    app(:multi_run) do |r|
+      r.multi_run do |prefix|
+        yielded = prefix
+      end
+    end
+
+    app.run("a"){Class.new(Roda).class_eval{route{"a1"}; app}}
+
+    body("/a").must_equal "a1"
+    yielded.must_equal "a"
+  end
+
+  it "allows removing dispatching to apps" do
+    app(:multi_run) do |r|
+      r.multi_run
+      "c"
+    end
+
+    app.run("a"){Class.new(Roda).class_eval{route{"a1"}; app}}
+    body("/a").must_equal 'a1'
+    app.run "a"
+    body("/a").must_equal 'c'
+  end
+
+  it "does not allow registering both app and app block in same call" do
+    app(:multi_run) do |r|
+      r.multi_run
+      "c"
+    end
+
+    proc{app.run("a", Class.new){}}.must_raise Roda::RodaError
+  end
+
+  it "registering app removes app block and vice versa" do
+    app(:multi_run) do |r|
+      r.multi_run
+      "c"
+    end
+
+    body("/a").must_equal 'c'
+    app.run "a", Class.new(Roda).class_eval{route{"a1"}; app}
+    body("/a").must_equal 'a1'
+    app.run("a"){Class.new(Roda).class_eval{route{"a2"}; app}}
+    body("/a").must_equal 'a2'
+    app.run "a", Class.new(Roda).class_eval{route{"a3"}; app}
+    body("/a").must_equal 'a3'
+    app.run "a"
+    body("/a").must_equal 'c'
+  end
+
+  it "supports both apps and app blocks" do
+    app(:multi_run) do |r|
+      r.multi_run
+      "c"
+    end
+
+    app.run "a", Class.new(Roda).class_eval{route{"a"}; app}
+    app.run("b"){Class.new(Roda).class_eval{route{"b"}; app}}
+    body("/a").must_equal 'a'
+    body("/b").must_equal 'b'
   end
 end
